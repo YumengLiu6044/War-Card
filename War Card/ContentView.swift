d55fa6c6ff556860097f5a5dbc424d465751b19b
Yumeng Liu <lym20041026@gmail.com>
2024-06-29 10:13:47 -0700

    Added the UI for the game

--- War Card/ContentView.swift
+++ War Card/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background-plain")
+                .ignoresSafeArea()
+            VStack{
+//                logo
+                Image("logo")
+                    .padding(.bottom, 50.0)
+                
+//                Hstack of cards
+                HStack{
+                    Image("back")
+                    Spacer()
+                        .frame(width: 50.0)
+                    Image("back")
+                }
+                .padding(.bottom, 50.0)
+                
+//                Deal button
+                Image("button")
+                    .padding(.bottom, 50.0)
+                
+//                Hstack of score
+                HStack{
+                    VStack{
+                        Text("Player")
+                            .padding(.bottom, 10)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                        .frame(width: 100.0)
+                    VStack{
+                        Text("CPU")
+                            .padding(.bottom, 10)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                }
+                .foregroundColor(/*@START_MENU_TOKEN@*/Color(hue: 1.0, saturation: 0.008, brightness: 0.987)/*@END_MENU_TOKEN@*/)
+                
+                
+            }
+            
+            
         }
-        .padding()
     }
 }
 
