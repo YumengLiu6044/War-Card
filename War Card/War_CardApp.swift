5827b619fad168a54e5d12158f43ca9517e8ae49
Yumeng Liu <lym20041026@gmail.com>
2024-06-29 09:50:03 -0700

    Initial Commit

---
+++ War Card/War_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_CardApp.swift
+//  War Card
+//
+//  Created by Yumeng Liu on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct War_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
